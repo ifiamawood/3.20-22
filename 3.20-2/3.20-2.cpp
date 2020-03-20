60d1a0e2ee0e283c855d5576314d79bbdad6ac62
ifiamawood <821371350@qq.com>
2020-03-20 15:19:58 +0800

    zuihou

--- 3.20-2/3.20-2.cpp
+++ 3.20-2/3.20-2.cpp
@@ -12,29 +12,27 @@ using namespace std;
 int main()
 {
 	cv::Mat a = imread("D:\\coin.png", 0);
-	cv::Mat b, out, status,e,otsu;
-	cv::threshold(a, b, 0, 255, THRESH_OTSU);
-	std::vector<Mat>SrcMatpart(otsu.channels());
-	split(otsu, SrcMatpart);
-	int z = cv::connectedComponentsWithStats(b,
+	cv::Mat d, out, s,e,otsu;
+	cv::threshold(a, d, 0, 255, THRESH_OTSU);
+	int z = cv::connectedComponentsWithStats(d,
 		out,
-		status,
+		s,
 		e,
 		8,
 		CV_32S);
 	for (int i = 0; i < z; i++) {
-		cout << "connectef Components NO." << i << endl;
-		cout << "pixels=" << status.at<int>(i, 4) << endl;
-		cout << "width=" << status.at<int>(i, 2) << endl;
-		cout << "height=" << status.at<int>(i, 3) << endl;
-		cout << endl;
+		Rect b;
+		b.x = s.at<int>(i, 0);
+		b.y = s.at<int>(i, 1);
+		b.width = s.at<int>(i, 2);
+		b.height = s.at<int>(i, 3);
+		rectangle(d, b, CV_RGB(255, 255, 255), 1, 8, 0);
 
 
 }
 	cout << "the total of connected Components=" << z - 1 << endl;
-	imshow("otsu", otsu); 
+	imshow("½á¹û",d ); 
 	waitKey(0);
-
     return 0;
 }
 
